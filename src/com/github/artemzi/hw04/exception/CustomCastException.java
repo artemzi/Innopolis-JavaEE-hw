d1e412a43a9bb90bc7ad1d0fd8dafb70bba757f8
Artem Zinoviev <arzinoviev@gmail.com>
2018-08-28 11:16:26 +0300

    Remove old packages

--- src/com/github/artemzi/hw04/exception/CustomCastException.java
+++
@@ -1,7 +0,0 @@
-package com.github.artemzi.hw04.exception;
-
-public class CustomCastException extends ClassCastException {
-    public CustomCastException(String msg) {
-        super("[ObjectBox] " + msg);
-    }
-}
